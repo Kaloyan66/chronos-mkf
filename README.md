6528ded99ed7bb98f9c7e5077eb0ebe5c7d9c796
Kaloyan66 <kaloqnv5@gmail.com>
2025-08-29 13:14:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chronos-mkf
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Kaloyan66/chronos-mkf)
---
+++ src/stripe-config.ts
@@ -0,0 +1,11 @@
+export const products = {
+  'Mein Kampf': {
+    priceId: 'price_1RxaN0CNmIXjMY5wRgLxza6n',
+    productId: 'prod_StN7zkbahaS7Ej',
+    name: 'Mein Kampf',
+    description: 'The Revolution Has Begun',
+    price: 10.00,
+    currency: 'USD',
+    mode: 'payment' as const,
+  },
+} as const;
