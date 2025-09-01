b32d1f9be7c33873ef45c76bcf565fa77fc72716
Kaloyan66 <kaloqnv5@gmail.com>
2025-09-01 19:45:50 +0300

    Update stripe-config.ts
    Price change to 7$

--- src/stripe-config.ts
+++ src/stripe-config.ts
@@ -1,10 +1,10 @@
 export const products = {
   'Mein Kampf': {
-    priceId: 'price_1RxaN0CNmIXjMY5wRgLxza6n',
+    priceId: 'price_1S2aoACNmIXjMY5wlGqkFRRu',
     productId: 'prod_StN7zkbahaS7Ej',
     name: 'Mein Kampf',
     description: 'The Revolution Has Begun',
-    price: 10.00,
+    price: 7.00,
     currency: 'USD',
     mode: 'payment' as const,
   },
